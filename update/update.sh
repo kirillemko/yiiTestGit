baeaf881f16d69ea0451c766d2fed0b8a1fda38a
emelyanenko_k <sadfsd@asdf.ru>
2019-06-26 10:02:20 +0300

    update

--- update/update.sh
+++ update/update.sh
@@ -1,2 +1,11 @@
 cd ..
+
+git reset --hard HEAD
+
 git pull
+
+composer update
+
+./yii migrate
+
+d2ac6285f918318175eb6fea69f46954769cafd6
--- web/update/update.php
+++ web/update/update.php
@@ -1,2 +1,6 @@
 <?php
-shell_exec('../update/update.sh');
+
+if( isset($_GET['secret']) && $_GET['secret'] == 'h2fg13hkg1f' ){
+    shell_exec('../update/update.sh');
+}
+
